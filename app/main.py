fcd2bcf2da01c66872dcbf503b81fce57e05a9fa
haileylisak <143550880+haileylisak@users.noreply.github.com>
2026-06-16 09:16:58 -0400

    main notes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from app.api import users, workspaces, records, jobs
 
+'''
+Initializes a new FastAPI web application
+This automatically builds my interactive Swagger documentation configuration page
+    - Swagger File: blueprint file that describes how the web API works in JSON or YAML
+'''
 app = FastAPI(
     title="Team Data Workspace API",
     version = "1.0.0",
     description = "PArt 1 Implementation"
 )
 
+'''
+app.include_router(): takes the isolated department files and plugs them into the main system
+    - also defines the URL architecture 
+'''
 app.include_router(users.router, tags=["Users"])
 app.include_router(workspaces.router, tags=["Workspaces"])
 
